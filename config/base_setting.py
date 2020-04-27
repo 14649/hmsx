6d1141ff30aabc7885f58fc2e5a4d272dd0648bc
14649 <1464953387@qq.com>
2020-04-27 17:34:30 +0800

    登录登出，首页渲染

--- config/base_setting.py
+++ config/base_setting.py
@@ -1,7 +1,16 @@
 # 设置服务器端口
 SERVER_PORT = 8999
-SQLALCHEMY_DATABASE_URI = 'mysql://ljw:950920@127.0.0.1/hmsx_db?charset=uft8'
+SQLALCHEMY_DATABASE_URI = 'mysql://ljw:950920@127.0.0.1/hmsx_db?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # cookie
 AUTH_COOKIE_NAME = '1903_hmsx'
+
+# 拦截器忽略规则
+IGNORE_URLS = [
+    "^/user/login"
+]
+IGNORE_CHECK_LOGIN_URLS = [
+    "^/static",
+    "^/favicon.ico"
+]
