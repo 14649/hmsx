ca06fb6ef25417305176e7e7e97e3cc17fc7b6d9
14649 <1464953387@qq.com>
2020-04-28 13:49:06 +0800

    添加修改账号

--- urls.py
+++ urls.py
@@ -1,9 +1,12 @@
 from application import app
 from web.controllers.user.User import router_user
 from web.controllers.index import router_index
+from web.controllers.account.Account import router_account
+
 
 # 拦截器的路由
 from web.interceptos.AuthInterceptor import *
 
 app.register_blueprint(router_user, url_prefix='/user')
 app.register_blueprint(router_index,url_prefix="/")
+app.register_blueprint(router_account,url_prefix="/account")
